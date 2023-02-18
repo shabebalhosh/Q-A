b018c033a4d7b9df1ab41e6e200c838d31138366
shabebalhosh <shabebalhosh.lb@gmail.com>
2023-02-18 14:46:36 +0200

    ./.

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const names = document.getElementById("name").value;
-const score = document.getElementById("score").value;
-const url = "http://127.0.0.1:5000/score";
-
-fetch(url, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-  body: `name=${names}&score=${score}`,
-})
-  .then((response) => response.text())
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
+document.getElementById('submitName').addEventListener('submit', function(e) {
+  e.preventDefault();
+  window.location.replace("questions.html");
+});
