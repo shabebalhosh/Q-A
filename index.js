567a8ed84929dd71fc48d6fbea8d3a3596842a8a
shabebalhosh <shabebalhosh.lb@gmail.com>
2023-02-19 12:20:00 +0200

    added question/answer func.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-document.getElementById("submitName").addEventListener("submit", function (e) {
-  e.preventDefault();
-  window.location.replace("questions.html");
+const PlayerName = document.getElementById("name");
+
+const nameForm = document.getElementById("submitName");
+nameForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  window.location.href = `questions.html?name=${name}`;
 });
