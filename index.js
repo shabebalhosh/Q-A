929b749fca99ea02e8490d1d8ba229804ba0704f
shabebalhosh <shabebalhosh.lb@gmail.com>
2023-03-01 18:10:49 +0200

    full-theme applied with major fixes and additions in html and js codes

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ document.getElementById("name").oninput = function () {
   this.value = this.value.replace(/[^\s,أ-ي]/gi, "");
 };
 
+const inputBox = document.getElementById("name");
+const pTag = document.querySelector(".center p");
+const p2Tag = document.querySelector(".center .p2");
+const title = document.querySelector(".start h1");
+
+inputBox.addEventListener("focus", function () {
+  pTag.classList.add("hidden");
+  p2Tag.classList.add("hidden");
+  title.classList.add("hidden");
+});
+
+inputBox.addEventListener("focusout", function () {
+  pTag.classList.remove("hidden");
+  p2Tag.classList.remove("hidden");
+  title.classList.remove("hidden");
+});
+
+document.getElementById("name").setAttribute("autocomplete", "off");
+
 nameForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const name = document.getElementById("name").value;
